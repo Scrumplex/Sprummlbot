8d323f1501f02a93196a762f5fe36d2111033a39
Sefa Eyeoglu <sefa6727@gmail.com>
2015-11-16 16:50:34 +0100

    Added Config function to Sprummlbot

--- src/ga/codesplash/scrumplex/sprummlbot/plugins/SprummlPlugin.java
+++ src/ga/codesplash/scrumplex/sprummlbot/plugins/SprummlPlugin.java
@@ -5,12 +5,14 @@ import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
 
 public interface SprummlPlugin {
 
-	public boolean init(String version);
+	boolean init(String version);
 	
-	public void end();
+	void end();
 	
-	public void handleEvent(SprummlEventType type, BaseEvent event);
+	void handleEvent(SprummlEventType type, BaseEvent event);
 	
-	public boolean handleCommand(Client sender, String command, String[] args);
-	
+	boolean handleCommand(Client sender, String command, String[] args);
+
+	String getName();
+
 }
--- src/ga/codesplash/scrumplex/sprummlbot/plugins/Sprummlbot.java
+++ src/ga/codesplash/scrumplex/sprummlbot/plugins/Sprummlbot.java
@@ -4,6 +4,10 @@ import com.github.theholywaffle.teamspeak3.TS3Api;
 
 import ga.codesplash.scrumplex.sprummlbot.SprummlbotLoader;
 import ga.codesplash.scrumplex.sprummlbot.Vars;
+import org.ini4j.Ini;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Sprummlbot {
 
@@ -13,5 +17,10 @@ public class Sprummlbot {
 	
 	public static PluginLoader getPluginLoader() {
 		return SprummlbotLoader.pl;
+	}
+
+	public static Ini createConfig(SprummlPlugin plugin) throws IOException {
+		File f = new File("plugins/" + plugin.getName(), "config.ini");
+		return new Ini(f);
 	}
 }
