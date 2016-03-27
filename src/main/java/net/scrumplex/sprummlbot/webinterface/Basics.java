f48e87fee690ecb931fc4af21941e2c5f3ce126e
Sefa Eyeoglu <sefa6727@gmail.com>
2016-03-27 18:33:49 +0200

    Increased Browser-Compatibility for the Redirector

--- src/main/java/net/scrumplex/sprummlbot/webinterface/Basics.java
+++ src/main/java/net/scrumplex/sprummlbot/webinterface/Basics.java
@@ -3,9 +3,9 @@ package net.scrumplex.sprummlbot.webinterface;
 class Basics {
 
     public static String getRedirection(String to) {
-        String sb = "<!doctype html><html>" + "<head>\n</head>\n" +
+        String sb = "<!DOCTYPE html><html>" + "<head>\n</head>\n" +
                 "<body>\n" +
-                "<script>location.replace(\"" + to + "\");\n" +
+                "<script type=\"text/javascript\">location.replace(\"" + to + "\");\n" +
                 "</script>\n" +
                 "</body>\n" +
                 "</html>";
