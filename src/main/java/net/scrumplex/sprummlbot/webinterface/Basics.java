cf4f611fcc865ecf5245c00faf9dcdd8c08fd5e9
Sefa Eyeoglu <sefa6727@gmail.com>
2016-03-27 18:34:59 +0200

    Added new line for script-tag

--- src/main/java/net/scrumplex/sprummlbot/webinterface/Basics.java
+++ src/main/java/net/scrumplex/sprummlbot/webinterface/Basics.java
@@ -5,7 +5,8 @@ class Basics {
     public static String getRedirection(String to) {
         String sb = "<!DOCTYPE html><html>" + "<head>\n</head>\n" +
                 "<body>\n" +
-                "<script type=\"text/javascript\">location.replace(\"" + to + "\");\n" +
+                "<script type=\"text/javascript\">\n" +
+                "location.replace(\"" + to + "\");\n" +
                 "</script>\n" +
                 "</body>\n" +
                 "</html>";
