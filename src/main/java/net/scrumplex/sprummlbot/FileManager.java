3984297af4149c406f293354da0c72c36525b94e
Sefa Eyeoglu <sefa6727@gmail.com>
2016-03-27 17:54:57 +0200

    Removed Apache-Commons License notice (no more in use)

--- src/main/java/net/scrumplex/sprummlbot/FileManager.java
+++ src/main/java/net/scrumplex/sprummlbot/FileManager.java
@@ -10,7 +10,6 @@ class FileManager {
     static void createLicensesFile() throws IOException {
         File f = new File("licenses.txt");
         String sb = "Open Source Licenses:\n" +
-                "Apache Commons Lang and Apache Commons Codec: http://www.apache.org/licenses/LICENSE-2.0\n" +
                 "Teamspeak-3-Java-API: http://www.gnu.org/licenses/gpl-3.0.en.html\n" +
                 "JSON: http://www.json.org/license.html\n";
         EasyMethods.writeToFile(f, sb);
