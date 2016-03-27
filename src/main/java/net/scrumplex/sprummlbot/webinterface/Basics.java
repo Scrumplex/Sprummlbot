8a8865c49c9a6f4d2de0a9b166bcb254594738a7
Sefa Eyeoglu <sefa6727@gmail.com>
2016-03-27 18:32:03 +0200

    Changed to location.replace to improve load-time

--- src/main/java/net/scrumplex/sprummlbot/webinterface/Basics.java
+++ src/main/java/net/scrumplex/sprummlbot/webinterface/Basics.java
@@ -5,7 +5,7 @@ class Basics {
     public static String getRedirection(String to) {
         String sb = "<!doctype html><html>" + "<head>\n</head>\n" +
                 "<body>\n" +
-                "<script> location.href = \"" + to + "\";\n" +
+                "<script>location.replace(\"" + to + "\");\n" +
                 "</script>\n" +
                 "</body>\n" +
                 "</html>";
